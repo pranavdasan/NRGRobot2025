dad30d9e4e4085541095943e39e280c68384d0f6
NRG948DriverStation <programming@nrg948.com>
2025-02-23 09:14:57 -0800

    Fixes coral and algae heights & angles

--- src/main/java/frc/robot/parameters/ElevatorLevel.java
+++ src/main/java/frc/robot/parameters/ElevatorLevel.java
@@ -12,14 +12,14 @@ import frc.robot.subsystems.Elevator;
 public enum ElevatorLevel {
   // TODO: Determine correct values for height, arm angles, and arm offsets for coral placement.
   STOWED(Elevator.STOWED_HEIGHT_FOR_PID, Math.toRadians(90), 0),
-  L1(0.26, Math.toRadians(95), .2), // Assumes we shoot the coral in-line with the L1 shelf
+  L1(0.15, Math.toRadians(36), .2), // Assumes we shoot the coral in-line with the L1 shelf
   L2(0.33, Math.toRadians(60), .2),
   L3(0.74, Math.toRadians(60), .2),
-  L4(1.29, Math.toRadians(50), .2),
+  L4(1.32, Math.toRadians(50), .2),
 
   // TODO: Determine correct values for height, arm angles, and arm offsets for algae removal.
-  AlgaeL2(0.25, 40, .2),
-  AlgaeL3(0.65, 40, .2);
+  AlgaeL2(0.25, Math.toRadians(40), .2),
+  AlgaeL3(0.65, Math.toRadians(40), .2);
 
   private final double elevatorHeight;
   private final double armAngle;
