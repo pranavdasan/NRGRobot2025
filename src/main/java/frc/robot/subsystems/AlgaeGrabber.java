9c9a36233b941644565e17e7afccc73ecfdf2631
NRG948DriverStation <programming@nrg948.com>
2025-02-25 20:28:35 -0800

    Implements velocity control on algae grabber
    Co-Authored-By: "Rhea Sadhwani" <rheasadhwani@outlook.com>

--- src/main/java/frc/robot/subsystems/AlgaeGrabber.java
+++ src/main/java/frc/robot/subsystems/AlgaeGrabber.java
@@ -8,59 +8,134 @@
 package frc.robot.subsystems;
 
 import static frc.robot.Constants.RobotConstants.CAN.TalonFX.ALGAE_GRABBER_MOTOR_ID;
-import static frc.robot.util.MotorDirection.COUNTER_CLOCKWISE_POSITIVE;
+import static frc.robot.Constants.RobotConstants.MAX_BATTERY_VOLTAGE;
+import static frc.robot.util.MotorDirection.CLOCKWISE_POSITIVE;
 import static frc.robot.util.MotorIdleMode.BRAKE;
 
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.nrg948.preferences.RobotPreferences;
+import com.nrg948.preferences.RobotPreferencesLayout;
+import com.nrg948.preferences.RobotPreferencesValue;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.networktables.GenericEntry;
+import edu.wpi.first.util.datalog.BooleanLogEntry;
+import edu.wpi.first.util.datalog.DataLog;
+import edu.wpi.first.util.datalog.DoubleLogEntry;
+import edu.wpi.first.wpilibj.DataLogManager;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.shuffleboard.BuiltInLayouts;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardLayout;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.parameters.MotorParameters;
 import frc.robot.util.MotorIdleMode;
+import frc.robot.util.RelativeEncoder;
 import frc.robot.util.TalonFXAdapter;
 
+@RobotPreferencesLayout(groupName = "AlgaeGrabber", row = 2, column = 0, width = 1, height = 2)
 public class AlgaeGrabber extends SubsystemBase implements ActiveSubsystem, ShuffleboardProducer {
+
+  @RobotPreferencesValue
+  public static final RobotPreferences.BooleanValue ENABLE_TAB =
+      new RobotPreferences.BooleanValue("AlgaeGrabber", "Enable Tab", false);
+
+  @RobotPreferencesValue
+  public static final RobotPreferences.DoubleValue INTAKE_VELOCITY =
+      new RobotPreferences.DoubleValue("AlgaeGrabber", "Intake Velocity", 1);
+
+  @RobotPreferencesValue
+  public static final RobotPreferences.DoubleValue OUTTAKE_VELOCITY =
+      new RobotPreferences.DoubleValue("AlgaeGrabber", "Outtake Velocity", 2);
+
+  private static final DataLog LOG = DataLogManager.getLog();
+
+  private static final double WHEEL_DIAMETER = Units.inchesToMeters(3);
+  private static final double GEAR_RATIO = 1.0;
+
+  private static final double METERS_PER_REVOLUTION = (WHEEL_DIAMETER * Math.PI) / GEAR_RATIO;
+  private static final double MAX_VELOCITY =
+      (MotorParameters.KrakenX60.getFreeSpeedRPM() * METERS_PER_REVOLUTION) / 60;
+
+  private static final double KS = 0.0656;
+  private static final double KV = (MAX_BATTERY_VOLTAGE - KS) / MAX_VELOCITY;
+
+  private static final double ERROR_TIME = 3.0;
 
   private final TalonFXAdapter motor =
       new TalonFXAdapter(
-          new TalonFX(ALGAE_GRABBER_MOTOR_ID), COUNTER_CLOCKWISE_POSITIVE, BRAKE, 1.0);
-  private double motorSpeed = 0;
+          new TalonFX(ALGAE_GRABBER_MOTOR_ID, "rio"),
+          CLOCKWISE_POSITIVE,
+          BRAKE,
+          METERS_PER_REVOLUTION);
+  private final RelativeEncoder encoder = motor.getEncoder();
+
+  private final SimpleMotorFeedforward feedforward = new SimpleMotorFeedforward(KS, KV);
+  private final PIDController pidController = new PIDController(1, 0, 0);
+  private final Timer outtakeTimer = new Timer();
+
+  private double goalVelocity = 0;
+  private double currentVelocity = 0;
   private boolean enabled;
-  private boolean hasAlgae;
+  private boolean hasAlgae = false;
+  private boolean hasError = false;
+
+  private DoubleLogEntry logCurrentVelocity =
+      new DoubleLogEntry(LOG, "/AlgaeGrabber/currentVelocity");
+  private DoubleLogEntry logGoalVelocity = new DoubleLogEntry(LOG, "/AlgaeGrabber/goalVelocity");
+  private BooleanLogEntry logHasAlgae = new BooleanLogEntry(LOG, "/AlgaeGrabber/hasAlgae");
+  private DoubleLogEntry logFeedForward = new DoubleLogEntry(LOG, "/AlgaeGrabber/feedforward");
+  private DoubleLogEntry logFeedBack = new DoubleLogEntry(LOG, "/AlgaeGrabber/feedback");
+  private DoubleLogEntry logVoltage = new DoubleLogEntry(LOG, "/AlgaeGrabber/voltage");
 
   /** Creates a new AlgaeGrabber. */
   public AlgaeGrabber() {}
 
-  private void setMotorSpeed(double speed) {
+  /** Sets the goal velocity in meters per second. */
+  public void setGoalVelocity(double velocity) {
     enabled = true;
-    motorSpeed = speed;
+    goalVelocity = velocity;
+    logGoalVelocity.append(goalVelocity);
   }
 
+  /** Intakes the coral. */
   public void intake() {
-    // TODO: test & determine safe maximum speed
-    setMotorSpeed(0.8);
+    setGoalVelocity(INTAKE_VELOCITY.getValue());
   }
 
-  public void hold() {
-    // TODO: test & determine motor power needed to hang on to game piece while moving
-    setMotorSpeed(0.2);
-  }
-
+  /** Outakes the coral. */
   public void outtake() {
-    setMotorSpeed(-0.8);
+    setGoalVelocity(OUTTAKE_VELOCITY.getValue());
+    outtakeTimer.restart();
   }
 
+  /** Updates hasError if stuckTimer exceeds 3 seconds. */
+  public void checkError() {
+    hasError = outtakeTimer.hasElapsed(ERROR_TIME);
+  }
+
+  /** Disables the subsystem. */
+  @Override
+  public void disable() {
+    enabled = false;
+    goalVelocity = 0;
+    logGoalVelocity.append(0);
+    outtakeTimer.stop();
+    outtakeTimer.reset();
+    motor.stopMotor();
+  }
+
+  /** Returns whether we have coral. */
   public boolean hasAlgae() {
     return hasAlgae;
   }
 
-  @Override
-  public void disable() {
-    enabled = false;
-    motor.stopMotor();
+  /** Returns hasError. */
+  public boolean hasError() {
+    return hasError;
   }
 
   @Override
@@ -70,27 +145,51 @@ public class AlgaeGrabber extends SubsystemBase implements ActiveSubsystem, Shuf
 
   @Override
   public void periodic() {
+    updateTelemetry();
     if (enabled) {
-      motor.set(motorSpeed);
+      double feedforward = this.feedforward.calculate(goalVelocity);
+      double feedback = pidController.calculate(currentVelocity, goalVelocity);
+      double motorVoltage = feedforward + feedback;
+      motor.setVoltage(motorVoltage);
+      checkError();
+
+      logFeedForward.append(feedforward);
+      logFeedBack.append(feedback);
+      logVoltage.append(motorVoltage);
     }
   }
 
+  /** Updates and logs the current sensors states. */
+  private void updateTelemetry() {
+    currentVelocity = encoder.getVelocity();
+
+    logHasAlgae.update(hasAlgae);
+    logCurrentVelocity.append(currentVelocity);
+  }
+
+  @Override
   public void addShuffleboardTab() {
-    // if (!ENABLE_TAB.getValue()) {
-    //   return;
-    // }
+    if (!ENABLE_TAB.getValue()) {
+      return;
+    }
 
     ShuffleboardTab rollerTab = Shuffleboard.getTab("Algae Grabber");
-
     ShuffleboardLayout statusLayout =
         rollerTab.getLayout("Status", BuiltInLayouts.kList).withPosition(0, 0).withSize(2, 4);
     statusLayout.addBoolean("Enabled", () -> enabled);
+    statusLayout.addDouble("Goal Velocity", () -> goalVelocity);
+    statusLayout.addDouble("Current Velocity", () -> currentVelocity);
+    statusLayout.addBoolean("Has Algae", () -> hasAlgae);
+    statusLayout.add("Max Velocity", MAX_VELOCITY);
 
     ShuffleboardLayout controlLayout =
         rollerTab.getLayout("Control", BuiltInLayouts.kList).withPosition(2, 0).withSize(2, 4);
-    GenericEntry speed = controlLayout.add("Speed", 0).getEntry();
+    GenericEntry intakeSpeed = controlLayout.add("Intake Speed", 0).getEntry();
     controlLayout.add(
-        Commands.runOnce(() -> setMotorSpeed(speed.getDouble(0)), this).withName("Set Speed"));
+        Commands.runOnce(() -> setGoalVelocity(intakeSpeed.getDouble(0)), this).withName("Intake"));
+    GenericEntry holdSpeed = controlLayout.add("Hold Speed", 0).getEntry();
+    controlLayout.add(
+        Commands.runOnce(() -> setGoalVelocity(holdSpeed.getDouble(0)), this).withName("Hold"));
     controlLayout.add(Commands.runOnce(this::disable, this).withName("Disable"));
   }
 }
